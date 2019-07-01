1cf0ebeff8d98fc902b91f2c0d329c496391504c
aleda <alessandro.damico2@stud.unifi.it>
2019-07-01 17:40:40 +0200

    Numeri random decimali funzionano

--- RadixSort.py
+++ RadixSort.py
@@ -16,12 +16,12 @@ def Binarize(array):
    array[i] = int(bin(array[i])[2:])
  return array
 
-# B numeri da ordinare
-def random_vect(B):
+# genera vettore random di B numeri a bit bit
+def random_vect(B,bit):
     A=[]
     random.seed(2)
     for i in range(B):
-      A.append(random.randint(0, 1024)) #10 bit
+      A.append(random.randint(0, 2**bit))
     return A
 
 #  dimensione del vettore dei numeri da ordinare
@@ -93,7 +93,7 @@ def CountingSortRadix(A, B, AIstr, max): #(S,D,B,max()) AIstr e' una matrice
 
 
 
-prova = [1274, 19, 3680, 92]
+prova = [115, 187, 173, 739, 346, 6301, 515, 434, 73, 324]
 prova2 = [31274758, 31274757, 21047829, 78123209, 21979403]
 # TODO domanda al prof:
 # 1. Posso variare il passo, ma come lo studio analiticamnente senza i bit? devo considerasre che python usa 24 bit per intero
@@ -108,13 +108,12 @@ def RadixSort(A,p,numberLen):
     C = [0]*len(A)
     for i in range(0, len(A)):
         B.append(str(A[i]))
-    wordl=B[1] #numero di cifre nel numero, si da per scontato sia un multiplo del passo
-    if len(wordl) % p == 0:
-      r = int(len(wordl)/p)
+    if numberLen % p == 0:
+      r = int(numberLen/p)
 
     else:
       # numero di volte che il ciclo verra ripetuto
-      r = int(round((len(wordl)/p)+1))
+      r = int(round((numberLen/p)+1))
     print("Totale passaggi:", r)
     print("Numeri da ordinare", B,"\n")
     #per ogni cifra ora creo le colonne parziali, k per r passi di dimensione p
@@ -125,19 +124,21 @@ def RadixSort(A,p,numberLen):
         B = NumberToStringVectBin(B, numberLen)
         #prendo p cifre alla volta da ordinare
         for j in range(0, len(B)):
-            S.append(B[j][(len(wordl)-p-k*p):(len(wordl)-k*p)])
+            S.append(B[j][(numberLen-p-k*p):(numberLen-k*p)])
         #print("s e' :", S)
         S = StringToNumberVect(S)
         B = StringToNumberVect(B)
         #S colonna da ordinare, B numeri da ordinare in toto, D colonna dei risultati
         B = CountingSortRadix(S, D, B, int(max(S)))
     end = timer()
-    print("Timer:")
+    print("Timer:",end-start)
     print("Ordine finale: ",B)
 
+ok = random_vect(10,10)
+#ok1 = Binarize(ok)
+print(ok)
 
-print(random_vect(10))
-RadixSort(prova, 2, 4)
+RadixSort(ok, 2, 4)
 
 
 #lista_binaria = list(map(bin,prova2))
